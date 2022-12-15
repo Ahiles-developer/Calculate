63a5e4ecc35206f1445b5300ef27939fb3489413
Ahiles <yakushev_v@rambler.ru>
2022-12-15 16:59:15 +0300

    Создал метод getString

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,49 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 /**
  * Yakushev V.S. 14.12.2022
  * */
 public class Main {
-    public static void main(String[] args) {
-        //Scanner sc = new Scanner(System.in);
-        calc("1 + 5");
-
+    public static void main(String[] args) throws Exception {
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println(calc(sc.nextLine()));*/
+        System.out.println(calc("1+5"));
+        System.out.println(calc("9-5"));
+        System.out.println(calc("9*5"));
+        System.out.println(calc("9/3"));
 
     }
 
-    public static String calc(String input) {
+    public static String calc(String input) throws Exception {
         String[] str;
-        if(input.contains(" + ")) {
-            str = input.split(" + ");
+        if (input.contains("+")) {
+            str = input.split("\\+");
+            input = getString(input, str);
+        } else if (input.contains("-")) {
+            str = input.split("-");
+            input = getString(input, str);
+        } else if (input.contains("*")) {
+            str = input.split("\\*");
+            input = getString(input, str);
+        } else if (input.contains("/")) {
+            str = input.split("/");
+            input = getString(input, str);
+        } else {
+            throw new Exception("Некорректное действие...");
+        }
+        return input;
+    }
+
+
+    private static String getString(String input, String[] str) {
+        if((Integer.parseInt(str[0]) < 10 && Integer.parseInt(str[0]) >= 1) &&
+            Integer.parseInt(str[1]) < 10 && Integer.parseInt(str[1]) >= 1) {
             Integer sum = Integer.parseInt(str[0]) + Integer.parseInt(str[1]);
             input = String.valueOf(sum);
+        } else {
+            System.out.println("Вы ввели число меньше 0, либо больше 10.");
         }
-
         return input;
     }
 }
